762a95dbe8e6579d70a90d55482b0fdfbdd6f0b7
O-O-O-O <ersovmatvej330@gmail.com>
2025-05-13 19:13:52 +0300

    Дополнил скрипт кнопок.

--- Assets/Scripts/ButtonMenu.cs
+++ Assets/Scripts/ButtonMenu.cs
@@ -1,11 +1,16 @@
-using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ButtonMenu : MonoBehaviour
 { 
-  [Header("КнопкаПродолжить")]
+   [Header("Сохранение глав")]
+  public string Chapter1Key = "Chapter1"; 
+  public string Chapter2Key = "Chapter2";
+  public string Chapter3Key = "Chapter3";
+  public string Chapter4Key = "Chapter4";
+
+  [Header("Кнопка Продолжить")]
   public Button ButContinue;
   [Header("Панели")] 
   public GameObject PanelContinue;
@@ -13,20 +18,29 @@ public class ButtonMenu : MonoBehaviour
   public GameObject PanelSettings;
   public GameObject PanelDevelopment;
   public GameObject PanelExit;
-    void Start()
+     void Start()
     {
-        if(PlayerPrefs.HasKey("Chapter1")) 
-        {
-          ButContinue.gameObject.SetActive(true);
-        } 
-        else 
-        {
-          ButContinue.gameObject.SetActive(false);
-        }
+    if (PlayerPrefs.HasKey(Chapter1Key) || PlayerPrefs.HasKey(Chapter2Key) || PlayerPrefs.HasKey(Chapter3Key) || PlayerPrefs.HasKey(Chapter4Key))
+       {
+         ButContinue.gameObject.SetActive(true); 
+       }
+       else
+       {
+         ButContinue.gameObject.SetActive(false); 
+       }
     }
     public void Continue()
     {
       SceneManager.LoadScene(1);
       Debug.Log("Кнопка нажата");
     }
+    public void NewGame()
+    { 
+      if(PlayerPrefs.HasKey("Chapter1")) 
+      {
+      PlayerPrefs.SetInt("Chapter1", 1);
+      SceneManager.LoadScene(1);
+      Debug.Log("Кнопка нажата");
+      }
+    }   
 }
