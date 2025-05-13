bcf7adf1368ec7b546382aa13c87eb2d9e958130
O-O-O-O <ersovmatvej330@gmail.com>
2025-05-13 18:31:36 +0300

    Сделал функцию отвечающую за активность кнопки продолжить.

--- Assets/Scripts/ButtonMenu.cs
+++ Assets/Scripts/ButtonMenu.cs
@@ -1,11 +1,28 @@
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonMenu : MonoBehaviour
-{
+{ 
+  [Header("КнопкаПродолжить")]
+  public Button ButContinue;
+  [Header("Панели")] 
+  public GameObject PanelContinue;
+  public GameObject PanelNewGame;
+  public GameObject PanelSettings;
+  public GameObject PanelDevelopment;
+  public GameObject PanelExit;
     void Start()
     {
-        
+        if(PlayerPrefs.HasKey("Chapter1")) 
+        {
+          ButContinue.gameObject.SetActive(true);
+        } 
+        else 
+        {
+          ButContinue.gameObject.SetActive(false);
+        }
     }
     public void Continue()
     {
